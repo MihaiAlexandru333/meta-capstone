2f69786c697f074778808597e258282b588171b4
MihaiVIntila333 <durduvsmihai@yahoo.com>
2023-07-28 13:09:29 +0300

    add components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Main from './components/Main';
+import Nav from './components/Nav';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <>
+      <Header />
+      <Nav />
+      <Main />
+      <Footer />
+    </>
   );
 }
 
