d9968d6cc5204023195644bad1e3891f33e31920
MihaiVIntila333 <durduvsmihai@yahoo.com>
2023-07-29 10:05:38 +0300

    add assets folder

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,9 +1,41 @@
-import React from "react";
+import React from "react"
+import { Link } from "react-router-dom"
 
 export default function Nav() {
     return (
-        <nav>
-            <h1>Nav</h1>
-        </nav>
+        <>
+        <ul>
+          <li>
+            <Link to="/" className="a">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to="/" className="a">
+              About
+            </Link>
+          </li>
+          <li>
+            <Link to="/" className="a">
+              Menu
+            </Link>
+          </li>
+          <li>
+            <Link to="/reservations" className="a">
+              Reservations
+            </Link>
+          </li>
+          <li>
+            <Link to="/" className="a">
+              Order Online
+            </Link>
+          </li>
+          <li>
+            <Link to="/" className="a">
+              Login
+            </Link>
+          </li>
+        </ul>
+      </>
     )
 }
